d211dbcc6f73a56c91d439952ef35d8f4234e043
abust016 <abust016@fiu.edu>
2024-05-24 23:14:09 -0400

    correcting

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+//this is the app here
+const App = () => {
+    return (    
+        <div>
+            <h1>Hello, World!</h1>
+        </div>
+
+    )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react';
+import APP from './App';
+
+ReactDOM.render(<APP />, document.getElementById('root'))
